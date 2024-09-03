183b66634fa2c224abed64ef461f300294950305
Rasha Mughal <rashamughal058@gmail.com>
2024-09-03 18:48:54 +0500

    Adding all files of implicit intent

---
+++ app/src/main/java/com/example/explicitintent/MainActivity2.java
@@ -0,0 +1,35 @@
+package com.example.explicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        textView=findViewById(R.id.myTextView);
+        Intent intent=getIntent();
+        String textToReceive=intent.getStringExtra(Intent.EXTRA_TEXT);
+        textView.setText(textToReceive);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+
+}
